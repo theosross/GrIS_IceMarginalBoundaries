f60d26e433ffacc5cb7936e938eac913add72b3f
Theo Ross <97068201+theosross@users.noreply.github.com>
2022-11-17 17:50:58 -0800

    Add files via upload

---
+++ 1990/4_split_masked.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+This script splits the masked, classified mosaic into smaller
+tiles for processing
+'''
+
+import gdal
+import os
+
+# Define directory
+os.chdir("C:/Users/theoh/Dropbox (University of Oregon)/Oregon/Glacier/2019/")
+
+# Read files, set output
+in_path = "Classified/merged/"
+input_filename = 'masked.tif'
+out_path = "Classified/masked_split/"
+output_filename = 'image_'
+
+tile_size_x = 10000
+tile_size_y = 10000
+
+ds = gdal.Open(in_path + input_filename)
+band = ds.GetRasterBand(1)
+xsize = band.XSize
+ysize = band.YSize
+
+# Create files and export
+print('splitting masked image into smaller sizes for processing')
+for i in range(0, xsize, tile_size_x):
+    print(f"{i} out of {xsize}")
+    for j in range(0, ysize, tile_size_y):
+        com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + \
+            str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + \
+                " " + str(in_path) + str(input_filename) + " " + \
+                    str(out_path) + str(output_filename) + str(i) + "_" + \
+                        str(j) + ".tif"
+        os.system(com_string)
---
+++ 1990/6_ice-water_boundary_polylines.py
@@ -0,0 +1,68 @@
+'''
+This script is intended to be used within the QGIS python console.
+This script converts the detected ice-water raster boundaries into polylines.
+'''
+
+import processing
+import glob
+from qgis.core import (
+    QgsVectorLayer
+)
+
+filepath = "C:/Users/theoh/Dropbox (University of Oregon)/Oregon/Glacier/2019/Classified/"
+files = sorted(glob.glob(filepath + 'edges/water_ice/*.tif'))
+
+counter = 1
+for raster_file in files:
+
+    print(f'{counter} out of {len(files)}')
+
+    inRast = raster_file
+    outRast = filepath + f'edges/water_ice/masked/wil_masked_{counter}.tif'
+    
+    # Mask no data values
+    processing.run("gdal:translate", {'INPUT':inRast,\
+    'TARGET_CRS':None,'NODATA':0,'COPY_SUBDATASETS':False,\
+    'OPTIONS':'','EXTRA':'','DATA_TYPE':0,'OUTPUT':outRast})
+
+    inRast = outRast
+    outRast = filepath + f'edges/water_ice/thinned/wil_edge_thinned_{counter}.tif'
+    
+    # Thin raster cells to make clean polyline
+    processing.run("grass7:r.thin", \
+    {'input':inRast,'iterations':200,'output':outRast,\
+    'GRASS_REGION_PARAMETER':None,'GRASS_REGION_CELLSIZE_PARAMETER':0,\
+    'GRASS_RASTER_FORMAT_OPT':'','GRASS_RASTER_FORMAT_META':''})
+
+    inRast = outRast 
+    outVect = filepath + f'edges/water_ice/polyline/wil_edge_line_{counter}.shp'
+    
+    # Vectorize
+    processing.run("grass7:r.to.vect", \
+    {'input':inRast,'type':0,'column':'value',\
+    '-s':False,'-v':False,'-z':False,'-b':False,\
+    '-t':False,'output':outVect,'GRASS_REGION_PARAMETER':None,\
+    'GRASS_REGION_CELLSIZE_PARAMETER':0,'GRASS_OUTPUT_TYPE_PARAMETER':0,\
+    'GRASS_VECTOR_DSCO':'','GRASS_VECTOR_LCO':'',\
+    'GRASS_VECTOR_EXPORT_NOCAT':False})
+
+    inVect = outVect
+    outVect = filepath + f'edges/water_ice/reprojected/wil_line_repr_{counter}.shp'
+    
+    # Reporject to EPSG 3413
+    processing.run("native:reprojectlayer", \
+    {'INPUT':inVect,'TARGET_CRS':QgsCoordinateReferenceSystem('EPSG:3413'),\
+     'OPERATION':'+proj=pipeline +step +proj=unitconvert +xy_in=deg +xy_out=rad +step +proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +x_0=0 +y_0=0 +ellps=WGS84',\
+     'OUTPUT':outVect})
+
+    # Add vector layer to map
+    vlayer = QgsVectorLayer(outVect, "water_ice_vector", "ogr")
+    if not vlayer.isValid():
+        print("Layer failed to load!")
+    else:
+        QgsProject.instance().addMapLayer(vlayer)
+
+    counter += 1
+
+
+
